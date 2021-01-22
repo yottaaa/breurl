bcc6afdac4c2e5d93b2dcb5ab4c7d5acd88d3389
Erick A. Bautista <yotterick7@gmail.com>
2021-01-22 21:46:28 +0800

    Change SECRET_KEY value

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'by+e$#r_l^tx$hv!z(m!(8mz(t)@vy8j-vwb_nid*vhum*ryy$'
+#SECRET_KEY = 'by+e$#r_l^tx$hv!z(m!(8mz(t)@vy8j-vwb_nid*vhum*ryy$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
